64a870f7201c1c9db00467e480a5e0bb9835ddb3
rodolfogf <rodolfogf@gmail.com>
2021-07-20 22:49:06 -0300

    Quarto save

--- avaliacao2/lib/componentes/item_artista.dart
+++ avaliacao2/lib/componentes/item_artista.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:navegacao_roteiro/utils/rotas.dart';
+import 'package:avaliacao2/utils/rotas.dart';
+import '../model/artista.dart';
+import '../telas/tela_musica.dart';
 
-//importando meu modelo
-import '../models/artista.dart';
-
-//importando a tela de veículos
-import '../telas/tela_musicas.dart';
-
-class ItemMontadora extends StatelessWidget {
+class ItemArtista extends StatelessWidget {
   final Artista artista;
-
   const ItemArtista(this.artista);
 
-  void _selecionarMusica(BuildContext context) {
-    //vamos fazer um push de tela
-    // Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-    //   return TelaVeiculos();
-    // }));
-
+  void _selecinarMusica(BuildContext context) {
     Navigator.of(context).pushNamed(
       Rotas.MUSICAS,
       arguments: artista,
@@ -28,18 +18,12 @@ class ItemMontadora extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        _selecionarMusica(context);
+        _selecinarMusica(context);
       },
-      splashColor: Colors.orange,
+      splashColor: Colors.deepPurple,
       child: Container(
         padding: EdgeInsets.all(15),
-        child: Text(artista.nome),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            gradient: LinearGradient(colors: [
-              Color(int.parse('0x${artista.color}')).withOpacity(0.5),
-              Color(int.parse('0x${artista.color}')),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: Image.network(artista.artistaImagemPerfil),
       ),
     );
   }
--- avaliacao2/lib/componentes/item_musica.dart
+++ avaliacao2/lib/componentes/item_musica.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:navegacao_roteiro/utils/rotas.dart';
-
-//importando meu modelo
-import '../models/musicas.dart';
-
-//importando a tela de veículos
+import 'package:avaliacao2/utils/rotas.dart';
+import '../model/musica.dart';
 
 class ItemMusica extends StatelessWidget {
   final Musicas musica;
+  const ItemMusica(this.musica);
 
-  const ItemVeiculo(this.musica);
-
-  void _selecinarMusica(BuildContext context) {
+  void _selecionarMusica(BuildContext context) {
     Navigator.of(context).pushNamed(
       Rotas.DETALHES_MUSICAS,
       arguments: musica,
@@ -22,7 +17,7 @@ class ItemMusica extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        _selecinarMusica(context);
+        _selecionarMusica(context);
       },
       splashColor: Colors.orange,
       child: Card(
@@ -33,33 +28,12 @@ class ItemMusica extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Stack(
-                //Widget é opcional
                 children: <Widget>[
                   ClipRRect(
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(15),
                         topRight: Radius.circular(15)),
-                    child: Image.network(
-                      musica.imagem,
-                      height: 250,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
                   ),
-                  Positioned(
-                    bottom: 20,
-                    right: 10,
-                    child: Container(
-                      width: 300,
-                      color: Colors.black54,
-                      padding:
-                          EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                      child: Text(
-                        musica.modelo,
-                        style: TextStyle(fontSize: 26, color: Colors.white),
-                      ),
-                    ),
-                  )
                 ],
               ),
               Padding(
@@ -69,10 +43,10 @@ class ItemMusica extends StatelessWidget {
                   children: <Widget>[
                     Row(
                       children: [
-                        Icon(Icons.attach_money_sharp),
+                        Icon(Icons.info),
                         SizedBox(width: 5),
                         Text(
-                          "{musica.valor}",
+                          musica.musicasNome,
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         )
@@ -80,10 +54,10 @@ class ItemMusica extends StatelessWidget {
                     ),
                     Row(
                       children: [
-                        Icon(Icons.add_a_photo),
+                        Icon(Icons.timer),
                         SizedBox(width: 5),
                         Text(
-                          "${musica.combustivel}",
+                          musica.musicasDuracao,
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         )
@@ -91,15 +65,15 @@ class ItemMusica extends StatelessWidget {
                     ),
                     Row(
                       children: [
-                        Icon(Icons.calendar_today_outlined),
+                        Icon(Icons.music_note),
                         SizedBox(width: 5),
                         Text(
-                          "${musica.ano}",
+                          musica.musicasEstilo,
                           style: TextStyle(
                               fontSize: 20, fontWeight: FontWeight.bold),
                         )
                       ],
-                    )
+                    ),
                   ],
                 ),
               )
