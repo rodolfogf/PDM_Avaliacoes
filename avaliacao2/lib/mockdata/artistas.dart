2bc4ffff312c93d4110198dbe734d2ea44592ab5
rodolfogf <rodolfogf@gmail.com>
2021-07-21 00:00:27 -0300

    Sétimo save

--- avaliacao2/lib/mockdata/artistas.dart
+++ avaliacao2/lib/mockdata/artistas.dart
@@ -1,41 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../models/montadora.dart';
+import '../models/artista.dart';
 
-const MONTADORAS_MOCK = const [
-  Montadora(
-    id: 'vw',
-    nome: "Volkswagen",
-    color: '0000ff',
-  ),
-  Montadora(
-    id: 'ft',
-    nome: "Fiat",
-    color: 'ff0000',
-  ),
-  Montadora(
-    id: 'fr',
-    nome: "Ford",
-    color: 'ff8800',
-  ),
-  Montadora(
-    id: 'pg',
-    nome: "Peugeot",
-    color: 'ff00ff',
-  ),
-  Montadora(
-    id: 'fe',
-    nome: "Ferrari",
-    color: 'ff0000',
-  ),
-  Montadora(
-    id: 're',
-    nome: "Renault",
-    color: '0000ff',
-  ),
-  Montadora(
-    id: 'jp',
-    nome: "Jeep",
-    color: "000000",
-  ),
-];
+const MONTADORAS_MOCK = const [];
