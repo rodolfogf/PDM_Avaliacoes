20a7e42577da9950d7163b3ba50ce34af4807a4f
rodolfogf <rodolfogf@gmail.com>
2021-08-24 22:31:52 -0300

    Ajustes finais

--- avaliacao3/lib/services/authentication_service.dart
+++ avaliacao3/lib/services/authentication_service.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AuthService extends ChangeNotifier {
+class AuthenticationService extends ChangeNotifier {
   FirebaseAuth _auth = FirebaseAuth.instance;
   User? usuario;
   bool isLoading = true;
